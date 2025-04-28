a2b9d92e7253591e401fb5a8e9a24d962fd013b0
GonzaloRail <glaymem@unsa.edu.pe>
2025-04-28 18:36:49 -0500

    se agrego el formato hh:mm:ss

--- AlgoritmoDeCristian.py
+++ AlgoritmoDeCristian.py
@@ -5,6 +5,10 @@ def servidor_hora():
     """Simula un servidor que devuelve su hora actual."""
     return time.time()
 
+def formato_hora(segundos):
+    """Convierte un timestamp (segundos desde 1970) en formato HH:MM:SS."""
+    return time.strftime('%H:%M:%S', time.localtime(segundos))
+
 def algoritmo_cristian():
     """Cliente que sincroniza su reloj usando el Algoritmo de Cristian."""
     
@@ -30,10 +34,20 @@ def algoritmo_cristian():
     
     # Se estima el tiempo actual como:
     hora_estimada = hora_servidor + (rtt / 2)
+
     
+    print("\n#########TIEMPO EN SEGUNDOS#########\n")
+
     print(f"Hora local antes de sincronizar: {t0:.6f}")
     print(f"Hora recibida del servidor: {hora_servidor:.6f}")
     print(f"Hora local después de sincronizar: {hora_estimada:.6f}")
+    print("\n#########TIEMPO EN FORMATO HH:MM:SS########\n")
+
+    print(f"Hora local antes de sincronizar: {formato_hora(t0)}")
+    print(f"Hora recibida del servidor: {formato_hora(hora_servidor)}")
+    print(f"Hora local después de sincronizar: {formato_hora(hora_estimada)}")
+
+
 
 # Ejecutamos el algoritmo
 algoritmo_cristian()
